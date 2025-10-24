835cabc7d2ab8c25018395aebd3a6e42197265f1
Jamison Proctor <jamison.proctor@gmail.com>
2025-10-24 13:53:50 +0200

    update for debug

--- src/app/main.py
+++ src/app/main.py
@@ -46,10 +46,10 @@ def main():
 
 def schedule_jobs():
     # For testing, run every minute
-    # schedule.every(1).minutes.do(main)
+    # job = schedule.every(1).minutes.do(main)
 
     # For production, switch to midnight only
-    schedule.every().day.at("00:00").do(main)
+    job = schedule.every().day.at("00:00").do(main)
     logger.info(f"Scheduled job: {job.job_func.__name__} → next run at {job.next_run}")
 
     logger.info("Scheduler started. Waiting for tasks...")
